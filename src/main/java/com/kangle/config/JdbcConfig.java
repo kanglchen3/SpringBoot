38462d52fd8cf4b4a1b9c56f984a02f6a4217296
hasee <460195106@qq.com>
2020-10-18 23:20:59 +0800

    fix

--- src/main/java/com/kangle/config/JdbcConfig.java
+++ src/main/java/com/kangle/config/JdbcConfig.java
@@ -1,7 +1,7 @@
 package com.kangle.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import jdk.nashorn.internal.ir.CallNode;
+//import jdk.nashorn.internal.ir.CallNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
